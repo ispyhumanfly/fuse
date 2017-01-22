40cda6f0b9ff304ea6b3d7e59bfe1fed38aa4d86
Dan Stephenson <ispyhumanfly@gmail.com>
2017-01-22 03:33:12 -0500

    I've added a requirements.txt file and have begun configuring the setup.py script.

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
-#!/bin/env python
+#!/usr/bin/env python
+
+import sys
+
+if 'install' in sys.argv:
+
+    from distutils.spawn import find_executable
+
+    if find_executable('virtualenv'):
+
+        import subprocess
+
+        virtualenv = subprocess.Popen(['virtualenv', 'python_modules'], stdout=subprocess.PIPE)
+
+        if virtualenv.stdout.readlines():
+            pip = subprocess.Popen(['python_modules/bin/pip', 'install', '-r', 'requirements.txt'], stdout=subprocess.PIPE)
+            sys.stdout.write(virtualenv.stdout.readlines())
+            sys.exit(1)
+        else:
+            sys.stderr.write('An error occurred.\n')
+            sys.exit(0)
+
+    else:
+        sys.stderr.write('VirtualENV was not found on this system.\n')
+        sys.exit(0)
